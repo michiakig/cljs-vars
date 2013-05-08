d2d457f14f39cdc3c7a56bba2b46cbe8941a93ee
aki <spacemanaki@gmail.com>
2013-05-08 08:51:12 -0400

    update project.clj for lein2

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [marginalia "0.7.0"]
                  [enlive "1.0.0"]]
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
   :git-dependencies [["https://github.com/clojure/clojurescript.git" "master"]
                      ["https://github.com/clojure/clojure.git" "master"]]
   :main cljsvars.core)
